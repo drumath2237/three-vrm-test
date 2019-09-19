56af831fd30e70b4eff4fd37bf84e2d79cdd9a1e
drumath <2237k.drumath@gmail.com>
2019-09-19 17:28:29 +0900

    :sparkles: リングを一つ足していい感じにして、アンチエイリアシングして、モデルのボーンをちょっといじった

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,9 @@ window.addEventListener('DOMContentLoaded', ()=>{
     0.1,
     1000
   );
-  const renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
 
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setClearColor(0x4CAEAD,1.0);
@@ -20,16 +22,22 @@ window.addEventListener('DOMContentLoaded', ()=>{
   geometrys.push(
     new THREE.LatheGeometry(
       [
-        new THREE.Vector2(0.4, 0.01),
-        new THREE.Vector2(0.3,0.01)
-      ], 64, 1.0, 5.0
+        new THREE.Vector2(0.4, 0.001),
+        new THREE.Vector2(0.35,0.001)
+      ], 128, 1.0, 2.0
     ),
     new THREE.LatheGeometry(
       [
-        new THREE.Vector2(0.45, 0.01),
-        new THREE.Vector2(0.42, 0.01),
-      ], 64, 0, 2.0
-    )
+        new THREE.Vector2(0.45, 0.001),
+        new THREE.Vector2(0.42, 0.001),
+      ], 128, 0, 2.0
+    ),
+    new THREE.LatheGeometry(
+      [
+        new THREE.Vector2(0.33, 0.001),
+        new THREE.Vector2(0.23, 0.001)
+      ],128, Math.PI, 5.2
+    ),
   );
 
   /*
@@ -43,7 +51,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
   * meshes rotation function
   * use this for frame counter
   */
-  const meshes_loop_speeds = [0.04, 0.507];
+  const meshes_loop_speeds = [-0.04, 0.07, 0.02];
   let MeshesUpdate = () =>
     meshes.map( (mesh, index) =>
       mesh.rotation.y += meshes_loop_speeds[index]
@@ -67,11 +75,17 @@ window.addEventListener('DOMContentLoaded', ()=>{
         scene.add(vrm.scene);
         vrm.scene.rotation.y = Math.PI;
         
-        vrm.humanoid!.getBoneNode(VRMSchema.HumanoidBoneName.LeftUpperArm)!.rotation.z = Math.PI/2 - 0.3;
-        vrm.humanoid!.getBoneNode(VRMSchema.HumanoidBoneName.RightUpperArm)!.rotation.z = -(Math.PI/2 - 0.3);
+        vrm.humanoid!.getBoneNode(VRMSchema.HumanoidBoneName.LeftUpperArm)!.rotation.z = Math.PI/2 - 0.28;
+        vrm.humanoid!.getBoneNode(VRMSchema.HumanoidBoneName.LeftHand)!.rotation.z = 0.1;
+        vrm.humanoid!.getBoneNode(VRMSchema.HumanoidBoneName.RightUpperArm)!.rotation.z = -(Math.PI/2 - 0.28);
+        vrm.humanoid!.getBoneNode(VRMSchema.HumanoidBoneName.RightHand)!.rotation.z = -0.1;
+
         vrm.blendShapeProxy!.setValue(VRMSchema.BlendShapePresetName.Fun, 1);
         vrm.blendShapeProxy!.setValue(VRMSchema.BlendShapePresetName.Sorrow, 0.08);
         vrm.blendShapeProxy!.update();
+
+        vrm.lookAt!.target = camera;
+        // vrm.lookAt!.update();
         
         model = vrm;
         console.log(vrm);        
@@ -89,6 +103,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
     requestAnimationFrame(animate);
 
     MeshesUpdate();
+    model.lookAt!.update();
 
     renderer.render(scene, camera);
   }
